859686273863e3be9ee5b78d06cbe9386e53481f
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-01 11:51:39 +0530

    read,write,append to a file

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+
+// // read from file
+// fs.readFile('file.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+    
+//     console.log(data);
+// });
+
+// // write to file
+// fs.writeFile('file.txt', 'Hello, world!', (err) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+    
+//     console.log('File written successfully');
+// });
+
+// // append to file
+// fs.appendFile('file.txt', '\nHello, world!', (err) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+// });
